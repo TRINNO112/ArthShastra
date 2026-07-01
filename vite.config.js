d2fa57c08d4012a0163361158c016e5a765f5e8e
TRINNO112 <kaushtubh457@gmail.com>
2026-07-01 20:52:12 +0530

    M157

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 // Using SWC instead of Babel to avoid CSP eval() issues on GitHub Pages
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://arthshastra.netlify.app',
+      readable: true,
+      dynamicRoutes: [
+        '/',
+        '/lessons',
+        '/progress',
+        '/about'
+      ]
+    })
+  ],
   base: '/',
   build: {
     outDir: 'dist',
